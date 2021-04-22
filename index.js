d7f00b524ebfd9a8d7b468d3d069d7db05430327
Rafael Ramirez <rafa.yael@gmail.com>
2021-04-22 18:37:46 -0500

    ok

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const app = express()
-const port =  process.env.PORT
+const port = process.env.PORT
 
 const cors = require("cors")
 
-app.use(cors({
-     origin:"hola.com"
-}))
+var corsOptions = {
+  origin: "http://example.com",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 app.get("*", (req, res) => {
   res.send("Hello World!")
